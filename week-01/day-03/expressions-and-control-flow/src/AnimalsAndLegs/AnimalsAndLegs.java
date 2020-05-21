587e046ecefc3a37e6e4ea6d3ad9daf35e069880
sfu420 <freedom.of.p2p@gmail.com>
2020-05-21 17:32:46 +0200

    Add AnimalsAndLegs exercise

---
+++ week-01/day-03/expressions-and-control-flow/src/AnimalsAndLegs/AnimalsAndLegs.java
@@ -0,0 +1,17 @@
+package AnimalsAndLegs;
+
+import java.util.Scanner;
+
+public class AnimalsAndLegs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many chickens do you have? ");
+        int chicken = scanner.nextInt();
+
+        System.out.print("How many pigs do you have? ");
+        int pig = scanner.nextInt();
+
+        System.out.println("All the legs animals have: " + (2 * chicken + 4 * pig));
+    }
+}
