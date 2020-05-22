45281d3e532133c4a2f005b6d12b51f9adf4e869
sfu420 <freedom.of.p2p@gmail.com>
2020-05-22 11:24:58 +0200

    Add OddEven exercise

---
+++ week-01/day-03/expressions-and-control-flow/src/OddEven/OddEven.java
@@ -0,0 +1,18 @@
+package OddEven;
+
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please give me a number: ");
+        int num = scanner.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("This number is even!");
+        } else {
+            System.out.println("This number is odd!");
+        }
+    }
+}
