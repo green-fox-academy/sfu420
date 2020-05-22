fe6149a128140c3b6038b5c7fb466f9da5e279d7
sfu420 <freedom.of.p2p@gmail.com>
2020-05-22 14:08:59 +0200

    Fix CodeHours according to Johny's suggestions

--- week-01/day-03/expressions-and-control-flow/src/CodingHours/CodingHours.java
+++ week-01/day-03/expressions-and-control-flow/src/CodingHours/CodingHours.java
@@ -2,8 +2,14 @@ package CodingHours;
 
 public class CodingHours {
   public static void main(String[] args) {
-    int c = 17 * 5 * 6;
+    int numberOfWorkingWeeks = 17;
+    int daysInAWeek = 5;
+    int workingHours = 6;
+    int weeklyAverageWorkHours = 52;
+    int c = numberOfWorkingWeeks * daysInAWeek * workingHours;
+
     System.out.println("Coding Hours: " + c);
-    System.out.println("Average coding hours in percent: " + (float) c / (17 * 52) * 100 + "%");
+    System.out.println("Average coding hours in percent: " +
+        (float) c / (numberOfWorkingWeeks * weeklyAverageWorkHours) * 100 + "%");
   }
 }
