833fe48890282cfafc7b1d8a211cdc8948ef1b1f
sfu420 <freedom.of.p2p@gmail.com>
2020-05-22 21:25:12 +0200

    Fix Typo

--- week-01/day-03/expressions-and-control-flow/src/SecondsInADay/SecondsInADay.java
+++ week-01/day-03/expressions-and-control-flow/src/SecondsInADay/SecondsInADay.java
@@ -5,11 +5,11 @@ public class SecondsInADay {
     int currentHours = 14;
     int currentMinutes = 34;
     int currentSeconds = 42;
-    int secInWeek = 86400;
+    int secInDay = 86400;
 
     int secInHours = currentHours * 60 * 60;
     int secInMinutes = currentMinutes * 60;
-    int remainingSecs = secInWeek - (secInHours + secInMinutes + currentSeconds);
+    int remainingSecs = secInDay - (secInHours + secInMinutes + currentSeconds);
 
     System.out.println("Remaining seconds in this day: " + remainingSecs);
   }
