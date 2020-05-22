f27e37dfd5aa7aa6787f49287fc6caa714d699e0
sfu420 <freedom.of.p2p@gmail.com>
2020-05-22 20:34:20 +0200

    Remove unnecessary line from DrawPyramid

--- week-01/day-03/expressions-and-control-flow/src/DrawPyramid/DrawPyramid.java
+++ week-01/day-03/expressions-and-control-flow/src/DrawPyramid/DrawPyramid.java
@@ -9,12 +9,11 @@ public class DrawPyramid {
     System.out.print("Please give me a number: ");
     int num = scanner.nextInt();
 
-    for (int i = 0; i < num; i++) {
-      for (int j = num; j > i + 1; j--) {
+    for (int i = 1; i <= num; i++) {
+      for (int j = 1; j <= num - i; j++) {
         System.out.print(" ");
       }
-      System.out.print("*");
-      for (int j = 0; j < i * 2; j++) {
+      for (int j = 1; j < (i * 2); j++) {
         System.out.print("*");
       }
       System.out.println();
