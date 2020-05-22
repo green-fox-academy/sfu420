807cfc524214923ad381c986bfd5cc355b767350
sfu420 <freedom.of.p2p@gmail.com>
2020-05-22 16:09:51 +0200

    Add DrawTriangle exercise

---
+++ week-01/day-03/expressions-and-control-flow/src/DrawTriangle/DrawTriangle.java
@@ -0,0 +1,19 @@
+package DrawTriangle;
+
+import java.util.Scanner;
+
+public class DrawTriangle {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Please give me a number: ");
+    int num = scanner.nextInt();
+
+    for (int i = 0; i < num; i++) {
+      for (int j = 0; j <= i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
