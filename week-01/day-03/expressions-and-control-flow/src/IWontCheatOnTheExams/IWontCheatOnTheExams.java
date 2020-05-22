7b9f5d731be81b8c7fcebb346a477d1a5890ae3e
sfu420 <freedom.of.p2p@gmail.com>
2020-05-22 15:30:17 +0200

    Add IWontCheatOnTheExams exercise

---
+++ week-01/day-03/expressions-and-control-flow/src/IWontCheatOnTheExams/IWontCheatOnTheExams.java
@@ -0,0 +1,9 @@
+package IWontCheatOnTheExams;
+
+public class IWontCheatOnTheExams {
+  public static void main(String[] args) {
+    for (int i = 0; i < 100; i++) {
+      System.out.println("I won't cheat on the exam!" + i);
+    }
+  }
+}
