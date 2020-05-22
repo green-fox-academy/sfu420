ebd6ae95f2318ced2fc5b7a8d4ae00c88bb386c3
sfu420 <freedom.of.p2p@gmail.com>
2020-05-22 15:12:10 +0200

    Fix SecondsInADay according to Johny's suggestions

--- week-01/day-03/expressions-and-control-flow/src/SecondsInADay/SecondsInADay.java
+++ week-01/day-03/expressions-and-control-flow/src/SecondsInADay/SecondsInADay.java
@@ -5,10 +5,11 @@ public class SecondsInADay {
     int currentHours = 14;
     int currentMinutes = 34;
     int currentSeconds = 42;
+    int secInWeek = 86400;
 
     int secInHours = currentHours * 60 * 60;
     int secInMinutes = currentMinutes * 60;
-    int remainingSecs = 86400 - (secInHours + secInMinutes + currentSeconds);
+    int remainingSecs = secInWeek - (secInHours + secInMinutes + currentSeconds);
 
     System.out.println("Remaining seconds in this day: " + remainingSecs);
   }
