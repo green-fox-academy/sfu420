03e938784fc3e414c34272a8a6027fcf3ce4de73
sfu420 <freedom.of.p2p@gmail.com>
2020-05-25 15:39:26 +0200

    Add Greet exercise

---
+++ week-02/day-01/functions/src/Greet/Greet.java
@@ -0,0 +1,13 @@
+package Greet;
+
+public class Greet {
+  public static void main(String[] args) {
+    String al = "Green Fox";
+
+    greet(al);
+  }
+
+  public static void greet(String name) {
+    System.out.println("Greetings dear, " + name);
+  }
+}
