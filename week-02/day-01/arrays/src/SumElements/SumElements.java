8c34edec5d53f153389aae12ca0abfe722ca0f8a
sfu420 <freedom.of.p2p@gmail.com>
2020-05-25 13:54:28 +0200

    Add SumElements exercise

---
+++ week-02/day-01/arrays/src/SumElements/SumElements.java
@@ -0,0 +1,9 @@
+package SumElements;
+
+public class SumElements {
+  public static void main(String[] args) {
+    int[] numbers = {54, 23, 66, 12};
+
+    System.out.println("Sum of the second and third elements: " + (numbers[1] + numbers[2]));
+  }
+}
