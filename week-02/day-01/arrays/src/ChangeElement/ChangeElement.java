b5cbb038046cf334c04de7b0f6db98291ddd8d3f
sfu420 <freedom.of.p2p@gmail.com>
2020-05-25 13:57:16 +0200

    Add ChangeElement exercise

---
+++ week-02/day-01/arrays/src/ChangeElement/ChangeElement.java
@@ -0,0 +1,13 @@
+package ChangeElement;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class ChangeElement {
+  public static void main(String[] args) {
+
+    int[] numbers = {1, 2, 3, 8, 5, 6};
+
+    numbers[3] = 4;
+    System.out.println(numbers[3]);
+  }
+}
