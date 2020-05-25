3100cc007fc1d7638e944cbec40a3567cad739e6
sfu420 <freedom.of.p2p@gmail.com>
2020-05-25 14:36:17 +0200

    Add AppendA exercise

---
+++ week-02/day-01/arrays/src/AppendA/AppendA.java
@@ -0,0 +1,11 @@
+package AppendA;
+
+public class AppendA {
+  public static void main(String[] args) {
+    String[] animals = {"koal", "pand", "zebr"};
+
+    for (int i = 0; i < animals.length; i++) {
+      animals[i] = animals[i] + "a";
+    }
+  }
+}
