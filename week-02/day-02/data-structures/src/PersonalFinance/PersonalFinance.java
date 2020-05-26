63c024e7b54066444103a1b19aefe753130ddf9c
sfu420 <freedom.of.p2p@gmail.com>
2020-05-26 18:26:05 +0200

    Some cosmetic change

--- week-02/day-02/data-structures/src/PersonalFinance/PersonalFinance.java
+++ week-02/day-02/data-structures/src/PersonalFinance/PersonalFinance.java
@@ -9,7 +9,7 @@ public class PersonalFinance {
     ArrayList<Integer> myExpenses = new ArrayList<>(Arrays.asList(500, 1000, 1250, 175, 800, 120));
     int sum = myExpenses.stream().mapToInt(Integer::intValue).sum();
     float average = (float) sum / myExpenses.size();
-    
+
     System.out.println("Sum: " + sum);
     System.out.println("Min expense: " + Collections.min(myExpenses));
     System.out.println("Max expense: " + Collections.max(myExpenses));
