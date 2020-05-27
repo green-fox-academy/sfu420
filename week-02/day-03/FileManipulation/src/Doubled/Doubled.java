3f0b67395d467d004c3a676c1fddc7a4695f2a04
sfu420 <freedom.of.p2p@gmail.com>
2020-05-27 16:50:05 +0200

    Add Doubled exercise

---
+++ week-02/day-03/FileManipulation/src/Doubled/Doubled.java
@@ -0,0 +1,40 @@
+// Create a method that decrypts the duplicated-chars.txt
+
+package Doubled;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Doubled {
+  public static void main(String[] args) {
+    String filename = "duplicated-chars.txt";
+    String decryptedFile = "decrypted.txt";
+
+    deduplication(filename, decryptedFile);
+  }
+
+  private static void deduplication(String filename, String decryptedFile) {
+    try {
+      Path filePath = Paths.get(filename);
+      List<String> lines = Files.readAllLines(filePath);
+      List<String> newLines = new ArrayList<>();
+      for (String line : lines) {
+        StringBuilder decryptedLine = new StringBuilder();
+        char[] charsByLine = line.toCharArray();
+        for (int i = 0; i < charsByLine.length; i = i + 2) {
+          decryptedLine.append(charsByLine[i]);
+        }
+        newLines.add(decryptedLine.toString());
+      }
+      Path newFilePath = Paths.get(decryptedFile);
+      Files.write(newFilePath, newLines);
+      System.out.println("Decryption was successful!");
+    } catch (IOException e) {
+      System.out.println("File not found");
+    }
+  }
+}
