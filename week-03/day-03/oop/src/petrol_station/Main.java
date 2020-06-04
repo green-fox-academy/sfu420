b464c709f78a75960b478224dd5c4bea9ef945f8
sfu420 <freedom.of.p2p@gmail.com>
2020-06-04 11:23:48 +0200

    Update Petrol Station exercise

--- week-03/day-03/oop/src/petrol_station/Main.java
+++ week-03/day-03/oop/src/petrol_station/Main.java
@@ -3,6 +3,10 @@ package petrol_station;
 public class Main {
   public static void main(String[] args) {
     Car car = new Car();
+    Station station = new Station();
 
+    System.out.println("Car's actual fuel state: " + car.gasAmount);
+    station.refill(car);
+    System.out.println("Car's actual fuel state: " + car.gasAmount);
   }
 }
