5948fb660448e7f8c1c53abcd259ad74dd200bc0
sfu420 <freedom.of.p2p@gmail.com>
2020-06-29 14:47:47 +0200

    Print average of odd numbers

---
+++ week-06/day-01/src/exercise4/Main.java
@@ -0,0 +1,15 @@
+package exercise4;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+
+    numbers.stream()
+        .filter(x -> x % 2 != 0)
+        .mapToInt(x -> x).average()
+        .ifPresent(System.out::println);
+  }
+}
