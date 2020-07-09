0c8118b6e3bf4cf94e7e383b3d2bb1f1095c78ec
sfu420 <freedom.of.p2p@gmail.com>
2020-07-09 13:14:11 +0200

    Add Login Page

--- week-07/day-04/foxclub/src/main/java/com/greenfox/foxclub/controller/MainController.java
+++ week-07/day-04/foxclub/src/main/java/com/greenfox/foxclub/controller/MainController.java
@@ -1,13 +1,34 @@
 package com.greenfox.foxclub.controller;
 
+import java.util.Optional;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
+
   @GetMapping("/")
-  public String information() {
+  public String information(@RequestParam Optional<String> name, Model model) {
+    if (name.isPresent() && !name.get().isEmpty()) {
+      model.addAttribute("name", name);
+    } else {
+      return "redirect:/login";
+//      model.addAttribute("name", "Anonymous");
+    }
     return "index";
+  }
+
+  @GetMapping("/login")
+  public String loginPage() {
+    return "login";
+  }
+
+  @PostMapping("/login")
+  public String getName(@RequestParam String name) {
+    return "redirect:/?name=" + name;
   }
 }
