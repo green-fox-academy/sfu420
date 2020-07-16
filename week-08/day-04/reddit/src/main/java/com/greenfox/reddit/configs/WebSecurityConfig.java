11aa8ca83d0beb5d915d2ac37cfc19e77d6a232a
sfu420 <freedom.of.p2p@gmail.com>
2020-07-17 00:21:15 +0200

    Add reddit logo to login/signup

--- week-08/day-04/reddit/src/main/java/com/greenfox/reddit/configs/WebSecurityConfig.java
+++ week-08/day-04/reddit/src/main/java/com/greenfox/reddit/configs/WebSecurityConfig.java
@@ -52,6 +52,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   public void configure(WebSecurity web) throws Exception {
-    web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/img/**");
   }
 }
